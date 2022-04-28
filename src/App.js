b0b4ee430433cbcef151483090fd0a98e8dfcb03
Yami <cursedlawliet@gmail.com>
2022-04-28 20:33:54 +0300

    startup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      
+    </>
   )
-}
+};
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <h2>Feedback UI</h2>
+      </div>
+    </header>
+  )
+}
+
+export default Header
