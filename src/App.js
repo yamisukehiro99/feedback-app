e0fff4fdecee4f5eb8cc67c832fb73f2f90ab97e
Yami <cursedlawliet@gmail.com>
2022-05-01 22:19:00 +0300

    adding animations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 import FeedbackData from './data/FeedbackData'
 import { useState } from 'react';
 import Header from './components/Header.jsx'
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
   const deleteHandler = (id) => {
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,24 +1,39 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
+import { motion, AnimatePresence } from "framer-motion";
 const FeedbackList = ({ feedback, deleteHandler }) => {
 	if (!feedback || feedback.length === 0) {
 		return <p>No Feedback Yet</p>;
 	}
-  const reviews = feedback.map((item) => (
-    <FeedbackItem key={item.id} item={item} deleteHandler={deleteHandler}/>
-  ))
+	const reviews = feedback.map((item) => (
+		<motion.div
+			key={item.id}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+		>
+			<FeedbackItem key={item.id} item={item} deleteHandler={deleteHandler} />
+		</motion.div>
+	));
 	return (
 		<div className="feedback-list">
-			{reviews}
+			<AnimatePresence>{reviews}</AnimatePresence>
 		</div>
 	);
 };
+//   const reviews = feedback.map((item) => (
+//     <FeedbackItem key={item.id} item={item} deleteHandler={deleteHandler}/>
+//   ))
+// 	return (
+// 		<div className="feedback-list">
+// 			{reviews}
+// 		</div>
+// 	);
+// };
 
 export default FeedbackList;
 
 FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape
-  )
-}
+	feedback: PropTypes.arrayOf(PropTypes.shape),
+};
