a4d51a0e2d97e5b36a83b4cc261d22fffcf5aee2
Yami <cursedlawliet@gmail.com>
2022-05-05 21:49:41 +0300

    edited App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
 import AboutIconLink from './components/AboutIconLink';
-import Card from './shared/Card';
 import { FeedbackProvider } from './context/FeedbackContext';
 const App = () => {
   return (
